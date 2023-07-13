c472c840ad9fc108477dba1abb6acec1dc8e5492
Martha Nassimba <marthanassimba@gmail.com>
2023-07-13 17:25:48 +0300

    first commit

---
+++ frontend/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.login, name="Login"),
+    path('dashboard/', views.home, name="Dashboard"),
+    path('students/', views.students, name="Students"),
+]
