51e71cd7b0408a7570d25757b091027feec05e20
deniskalule <denisjothamzkalule@gmail.com>
2023-07-17 13:56:52 +0300

    student list and part of student add

--- frontend/static/js/scripts.js
+++ frontend/static/js/scripts.js
@@ -1,9 +1,19 @@
+// datatables
+// $(document).ready(function() {
+    new DataTable("#table", {
+        "aLengthMenu" : [[3,5,10,25,-1],[3,5,10,25,'All']],
+        "iDisplayLength": 3
+    });
+// });
+
+// datatables
+
 // handling sidebar menu click
 
 
 $('.submenu').css({
     'display' : 'none',
-})
+});
 $('.main-menu').click(function (e) { 
     e.preventDefault();
     $('.chevron').toggle();
@@ -13,4 +23,6 @@ $('.main-menu').click(function (e) {
     
 });
 // handling sidebar menu click
+
+
 
