bc267976e719074a30a7ec6a89309aae7fd6496b
Martha Nassimba <marthanassimba@gmail.com>
2023-07-21 13:55:02 +0300

    worked on classes and subjects

--- frontend/urls.py
+++ frontend/urls.py
@@ -12,7 +12,13 @@ urlpatterns = [
     path('teacherlist/', views.teacherList, name="Teachers List"),
 
     path('addusers/', views.addUsers, name="Add User"),
-    path('userslist/', views.usersList, name="View List"),    
+    path('userslist/', views.usersList, name="View List"),
+
+    path('addclass/', views.addClass, name="Add Class"),
+    path('classlist/', views.classList, name=" View Classes"), 
+
+    path('addsubject/', views.addSubject, name="Add Subject"),
+    path('subjectlist/', views.subjectList, name=" View Subjects"),    
 
     path('showstudent/', views.showStudent, name="Student details"),
 
