6665006a26db6cb7d8a25c0579dc5b1c47e7d9b7
Martha Nassimba <marthanassimba@gmail.com>
2023-07-25 13:23:28 +0300

    working on setup of teacher app

--- frontend/static/js/scripts.js
+++ frontend/static/js/scripts.js
@@ -3,7 +3,7 @@
 // $(document).ready(function() {
     new DataTable("#table", {
         responsive: true,
-        scrollX: true,
+        // scrollX: true,
         "aLengthMenu" : [[3,5,10,25,-1],[3,5,10,25,'All']],
         "iDisplayLength": 3,
         
