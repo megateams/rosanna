d23e2232c90b23cf01e869c17afd2bccbfb75e92
mozesaaron <mozesmpanga@gmail.com>
2023-07-24 22:00:17 -0700

    classes and subjects backend frontend connections

--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+from .models import *
 
 # Create your views here.
 def home(request):
@@ -9,3 +11,228 @@ def login(request):
 
 def students(request):
     return render(request, 'frontend/students.html')
+
+# students views
+def studentsList(request):
+    return render(request, 'frontend/student/studentsList.html')
+
+def studentsAdd(request):
+    return render(request, 'frontend/student/studentsAdd.html')
+
+
+# teachers views
+def teacherAdd(request):
+    return render(request,'frontend/staff/teacherAdd.html')
+
+def teacherList(request):
+    return render(request,'frontend/staff/teacherList.html')
+# teachers views
+
+def showStudent(request):
+    return render(request, 'frontend/student/showStudent.html')
+# students views
+
+# support staff views
+def supportstaffAdd(request):
+      return render(request, 'frontend/staff/supportstaffAdd.html')
+
+# Send the registration staff details to and retrieve from database 
+def supportstaffreg(request):
+    # retrieve data from request
+    if request.method == 'POST':
+        fullname =request.POST.get('fullname')
+        contact =request.POST.get('contact')
+        email =request.POST.get('email')
+        address =request.POST.get('address')
+        gender =request.POST.get('gender')
+        dob =request.POST.get('dob')
+        qualification =request.POST.get('qualification')
+        position =request.POST.get('position')
+        
+        #create support staff object and submit it in the database
+        supportStaffReg =Supportstaff.objects.create(
+            fullname =fullname,
+            contact=contact,
+            email=email,
+            address=address,
+            gender=gender,
+            dob=dob,
+            qualification=qualification,
+            position=position            
+        )
+        supportStaffReg.save()
+        messages.success(request, 'Data successfully added!')
+                
+        # Redirect to the registration page for support staff after successful data addition
+        return redirect('AddSupportstaff')
+    
+def showsubjects(request):
+    subjects = Subjects.objects.all()
+    return render(request , 'frontend/academics/subjects.html' , {'subjects' : subjects})
+
+def addsubjectsform(request):
+    return render(request, 'frontend/academics/addsubjects.html')
+
+def addsubject(request):
+    if request.method == 'POST':
+        subjectname = request.POST.get('subjectname')
+        subjectid = request.POST.get('subjectid')
+        classlevel = request.POST.get('classlevel')
+        subjecthead = request.POST.get('subjecthead')
+        
+        Subjects.objects.create(
+            subjectname = subjectname ,
+            subjectid = subjectid ,
+            classlevel = classlevel ,
+            subjecthead = subjecthead ,
+        )
+        
+        Subjects.save
+    return render(request , 'frontend/academics/subjects.html')
+    
+def supportstaffList(request):
+    # Retrieve all support staff data from the database
+    all_support_staff = Supportstaff.objects.all()
+    # Pass the data to the template for rendering
+    return render(request, 'frontend/staff/supportstaffList.html', {'support_staff': all_support_staff})
+    
+def showSupportstaff(request):
+    return render(request, 'frontend/staff/showSupportstaff.html')
+# support staff views
+
+def staff(request):
+    return render(request, 'frontend/staff.html')
+
+
+#moses code
+#students registration views
+def student(request):
+    if(request.method == 'POST'):
+        stdnumber = request.POST.get('stdnumber')
+        regdate = request.POST.get('regdate')
+        childname = request.POST.get('childname')
+        gender = request.POST.get('gender')
+        dob = request.POST.get('dob')
+        address = request.POST.get('address')
+        house = request.POST.get('house')
+        studentclass = request.POST.get('studentclass')
+        fathername = request.POST.get('fathername')
+        fcontact = request.POST.get('fcontact')
+        foccupation = request.POST.get('foccupation')
+        mothername = request.POST.get('mothername')
+        mcontact = request.POST.get('mcontact')
+        moccupation = request.POST.get('moccupation')
+        livingwith = request.POST.get('livingwith')
+        guardianname = request.POST.get('guardianname')
+        gcontact = request.POST.get('gcontact')
+
+        Student =student.objects.create(
+            stdnumber =stdnumber,            
+            regdate = regdate ,
+            childname = childname ,
+            gender = gender ,
+            dob = dob ,
+            address = address ,
+            house = house ,
+            studentclass = studentclass , 
+            fathername = fathername ,
+            fcontact = fcontact ,
+            foccupation = foccupation ,
+            mothername = mothername , 
+            mcontact = mcontact ,
+            moccupation = moccupation ,
+            livingwith = livingwith ,
+            guardianname = guardianname ,
+            gcontact = gcontact
+        )        
+        Student.save ()
+        
+        return HttpResponse('Registration Successful')
+
+def subjects(request):
+    if request.method == 'POST':
+        subjectnames = request.POST.get('subjectname')
+        subjectids = request.POST.get('subjectid')
+        classlevels = request.POST.get('classlevel')
+        subjectheads = request.POST.get('subjecthead')
+    
+        Subjects.objects.create(
+            subjectname = subjectnames , 
+            subjectid = subjectids , 
+            classlevel = classlevels , 
+            subjecthead = subjectheads
+        )
+    
+        Subjects.save
+    return HttpResponse(subjectnames)
+
+def showclasses(request):
+    classes = Schoolclasses.objects.all
+    return render(request , 'frontend/academics/showclasses.html' , {'classes':classes})
+
+def addclasses(request):
+    classes = Schoolclasses.objects.all
+    return render(request , 'frontend/academics/addclasses.html')
+    
+def schoolclasses(request):
+    if request.method == 'POST':
+        classname = request.POST.get('classname')
+        classid = request.POST.get('classid')
+        classteacher = request.POST.get('classteacher')
+        numofstds = request.POST.get('numofstds')
+    
+        Schoolclasses.objects.create(
+            classname = classname ,
+            classid = classid ,
+            classteacher = classteacher ,
+            numofstds = numofstds 
+        )
+    
+        Schoolclasses.save
+    return render(request , 'frontend/academics/showclasses.html')
+    
+def teachers(request):
+    if request.method == 'POST':
+        teacherid = request.POST.get('teacherid')
+        teachernames = request.POST.get('teachernames')
+        dob = request.POST.get('dob')
+        gender = request.POST.get('gender')
+        contact = request.POST.get('contact')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        classes = request.POST.get('classes')
+        joiningdate = request.POST.get('joiningdate')
+        position = request.POST.get('position')
+        subject = request.POST.get('subject')
+        qualification = request.POST.get('qualification')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        Teachers.objects.create(
+            teacherid = teacherid ,
+            teachernames = teachernames ,
+            dob = dob ,
+            gender = gender ,
+            contact = contact ,
+            email = email ,
+            address = address ,
+            classes = classes ,
+            joiningdate = joiningdate ,
+            position = position ,
+            subject = subject , 
+            qualification = qualification ,
+            username = username , 
+            password = password
+        )
+        supportStaffReg.save()
+        messages.success(request, f" Your data, {fullname} has been successfully added!")
+                
+        # Redirect to the registration page for support staff after successful data addition
+        return redirect('AddSupportstaff')
+   
+def supportstaffList(request):
+    # Retrieve all support staff data from the database
+    all_support_staff = Supportstaff.objects.all()
+    # Pass the data to the template for rendering
+    return render(request, 'frontend/staff/supportstaffList.html', {'support_staff': all_support_staff})
+        
