06618c99fba5f9f4cc11dca7f440b602896af0aa
Martha Nassimba <marthanassimba@gmail.com>
2023-08-31 08:38:08 +0300

    cleared the main branch

--- frontend/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.login, name="Login"),
-    path('dashboard/', views.home, name="Dashboard"),
-    path('students/', views.students, name="Students"),
-]
