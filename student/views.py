4cd4c78e85e88ca460c96d3eba8f66f366d66f5c
deniskalule <denisjothamzkalule@gmail.com>
2023-09-14 15:05:35 +0300

    worked on student marks

--- student/views.py
+++ student/views.py
@@ -7,6 +7,8 @@ from django.template.loader import render_to_string
 import os
 from xhtml2pdf import pisa
 from django.conf import settings
+from django.db.models.functions import Coalesce, Cast
+from django.db.models import Avg, F, FloatField 
 # Create your views here.
 
 # view for the login page
@@ -58,7 +60,7 @@ def dashboard(request):
     std_number = request.session['std_number']
 
     student = Student.objects.get(stdnumber=std_number)   
-    term_data = Term.objects.all()
+    term_data = Term.objects.all().first()
 
     return render(request, 'student/dashboard.html',{'term_data':term_data,'student':student})
 
@@ -72,4 +74,65 @@ def paymenthistory(request,std_number):
     students = Student.objects.get(stdnumber = std_number)
     fees_list = Fees.objects.filter(stdnumber=std_number)
     return render(request, 'student/paymenthistory.html',{'fees_list':fees_list,'student':students})
+
+def my_results(request, std_number, class_id):
+    student = Student.objects.get(stdnumber=std_number)
+    schoolclass = Schoolclasses.objects.get(classid=class_id)
+    term_data = Term.objects.all()
+    mark_types = Mark.MARK_TYPES
+
+    # Get all subjects
+    subjects = Subjects.objects.filter(schoolclasses=schoolclass)
+
+    # Initialize a dictionary to store marks for each term, year, and mark type combination
+    term_year_marks = {}
+
+    for term in term_data:
+        term_marks_data = []
+        for mark_type, _ in mark_types:
+            # Fetch all marks for the student, class, term, year, and mark type
+            marks = Mark.objects.filter(
+                class_name=schoolclass,
+                student_name=student.childname,
+                current_term=term.current_term,
+                current_year=term.current_year,
+                mark_type=mark_type,
+            )
+
+            # Calculate the average mark for each subject within this mark type
+            subject_marks = {}
+            for mark in marks:
+                subject_id = mark.subject.subjectid
+                if subject_id not in subject_marks:
+                    subject_marks[subject_id] = {
+                        'subject_name': mark.subject.subjectname,
+                        'total_marks': 0,
+                        'marks_count': 0,
+                    }
+                subject_marks[subject_id]['total_marks'] += mark.marks_obtained
+                subject_marks[subject_id]['marks_count'] += 1
+
+            # Calculate average marks for each subject and add to the term_marks_data list
+            for subject_id, data in subject_marks.items():
+                average_mark = data['total_marks'] / data['marks_count'] if data['marks_count'] > 0 else 0
+                term_marks_data.append({
+                    'subject_id': subject_id,
+                    'subject_name': data['subject_name'],
+                    'mark_type': mark_type,
+                    'average_mark': average_mark,
+                })
+
+        term_year = f"{term.current_term} - {term.current_year}"
+        term_year_marks[term_year] = term_marks_data
+
+    context = {
+        'student': student,
+        'schoolclass': schoolclass,
+        'mark_types': mark_types,
+        'term_data': term_data,
+        'term_year_marks': term_year_marks,
+        'subjects': subjects,
+    }
+
+    return render(request, 'student/my_results.html', context)
 
